3bb1e25a0f54830ddb8fe19a9234eab196bc1b2f
bhallaji-vks <bhallaji.vks@gmail.com>
2017-04-06 07:44:22 +0000

    Adding RFID-RPi Interfacing along with AWS IoT Services Integration

---
+++ basicPubSub/basicPubSub.py
@@ -0,0 +1,211 @@
+'''
+/*
+ * Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+ '''
+
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import sys
+import logging
+import time
+import getopt
+import RPi.GPIO as GPIO
+import MFRC522
+import signal
+
+
+continue_reading = True
+
+# Capture SIGINT for cleanup when the script is aborted
+def end_read(signal,frame):
+    global continue_reading
+    print "Ctrl+C captured, ending read."
+    continue_reading = False
+    GPIO.cleanup()
+
+# Hook the SIGINT
+signal.signal(signal.SIGINT, end_read)
+
+# Create an object of the class MFRC522
+MIFAREReader = MFRC522.MFRC522()
+
+# Welcome message
+print "Welcome to the MFRC522 data read example"
+print "Press Ctrl-C to stop."
+
+
+# Custom MQTT message callback
+def customCallback(client, userdata, message):
+	print("Received a new message: ")
+	print(message.payload)
+	print("from topic: ")
+	print(message.topic)
+	print("--------------\n\n")
+
+# Usage
+usageInfo = """Usage:
+
+Use certificate based mutual authentication:
+python basicPubSub.py -e <endpoint> -r <rootCAFilePath> -c <certFilePath> -k <privateKeyFilePath>
+
+Use MQTT over WebSocket:
+python basicPubSub.py -e <endpoint> -r <rootCAFilePath> -w
+
+Type "python basicPubSub.py -h" for available options.
+"""
+# Help info
+helpInfo = """-e, --endpoint
+	Your AWS IoT custom endpoint
+-r, --rootCA
+	Root CA file path
+-c, --cert
+	Certificate file path
+-k, --key
+	Private key file path
+-w, --websocket
+	Use MQTT over WebSocket
+-h, --help
+	Help information
+
+
+"""
+
+# Read in command-line parameters
+useWebsocket = False
+host = ""
+rootCAPath = ""
+certificatePath = ""
+privateKeyPath = ""
+try:
+	opts, args = getopt.getopt(sys.argv[1:], "hwe:k:c:r:", ["help", "endpoint=", "key=","cert=","rootCA=", "websocket"])
+	if len(opts) == 0:
+		raise getopt.GetoptError("No input parameters!")
+	for opt, arg in opts:
+		if opt in ("-h", "--help"):
+			print(helpInfo)
+			exit(0)
+		if opt in ("-e", "--endpoint"):
+			host = arg
+		if opt in ("-r", "--rootCA"):
+			rootCAPath = arg
+		if opt in ("-c", "--cert"):
+			certificatePath = arg
+		if opt in ("-k", "--key"):
+			privateKeyPath = arg
+		if opt in ("-w", "--websocket"):
+			useWebsocket = True
+except getopt.GetoptError:
+	print(usageInfo)
+	exit(1)
+
+# Missing configuration notification
+missingConfiguration = False
+if not host:
+	print("Missing '-e' or '--endpoint'")
+	missingConfiguration = True
+if not rootCAPath:
+	print("Missing '-r' or '--rootCA'")
+	missingConfiguration = True
+if not useWebsocket:
+	if not certificatePath:
+		print("Missing '-c' or '--cert'")
+		missingConfiguration = True
+	if not privateKeyPath:
+		print("Missing '-k' or '--key'")
+		missingConfiguration = True
+if missingConfiguration:
+	exit(2)
+
+# Configure logging
+logger = logging.getLogger("AWSIoTPythonSDK.core")
+logger.setLevel(logging.DEBUG)
+streamHandler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+streamHandler.setFormatter(formatter)
+logger.addHandler(streamHandler)
+
+# Init AWSIoTMQTTClient
+myAWSIoTMQTTClient = None
+if useWebsocket:
+	myAWSIoTMQTTClient = AWSIoTMQTTClient("basicPubSub", useWebsocket=True)
+	myAWSIoTMQTTClient.configureEndpoint(host, 443)
+	myAWSIoTMQTTClient.configureCredentials(rootCAPath)
+else:
+	myAWSIoTMQTTClient = AWSIoTMQTTClient("basicPubSub")
+	myAWSIoTMQTTClient.configureEndpoint(host, 8883)
+	myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+
+# AWSIoTMQTTClient connection configuration
+myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
+myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+# Connect and subscribe to AWS IoT
+myAWSIoTMQTTClient.connect()
+#myAWSIoTMQTTClient.subscribe("sdk/test/Python", 1, customCallback)
+#time.sleep(2)
+
+# Publish to the same topic in a loop forever
+loopCount = 0
+
+# This loop keeps checking for chips. If one is near it will get the UID and authenticate
+while continue_reading:
+    
+    # Scan for cards    
+    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+
+    # If a card is found
+    if status == MIFAREReader.MI_OK:
+        print "Card detected"
+    
+    # Get the UID of the card
+    (status,uid) = MIFAREReader.MFRC522_Anticoll()
+
+    # If we have the UID, continue
+    if status == MIFAREReader.MI_OK:
+
+        # Print UID
+        print "Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3])
+	myAWSIoTMQTTClient.publish("sdk/test/Python", "RFID "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3]), 1)
+	time.sleep(1)
+			#"Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3])
+            #print "welcome hari"
+        # This is the default key for authentication
+        key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
+        
+        # Select the scanned tag 
+        MIFAREReader.MFRC522_SelectTag(uid)
+
+        # Authenticate
+        status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
+
+        # Check if authenticated
+        if status == MIFAREReader.MI_OK:
+            MIFAREReader.MFRC522_Read(8)
+            MIFAREReader.MFRC522_StopCrypto1()
+        else:
+            print "Authentication error"
+
+#while True:
+#	myAWSIoTMQTTClient.publish("sdk/test/Python", "RFID " + str(loopCount), 1)
+#	loopCount += 1
+#	time.sleep(1)
+
+
+
+
+
+	
